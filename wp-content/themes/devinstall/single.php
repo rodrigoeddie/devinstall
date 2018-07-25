3ae1a40a91fdca3b65506634f38364117245ef9c
Luego 08 - Rodrigo de Melo <rodrigodemelo@luego.com.br>
2018-07-25 18:58:27 -0300

    Working...

--- wp-content/themes/devinstall/single.php
+++ wp-content/themes/devinstall/single.php
@@ -10,15 +10,19 @@ get_header(); ?>
 
 <?php get_template_part( 'template-parts/featured-image' ); ?>
 <div class="main-container">
-	<div class="main-grid">
-		<main class="main-content">
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php get_template_part( 'template-parts/content', '' ); ?>
-				<?php the_post_navigation(); ?>
-				<?php comments_template(); ?>
-			<?php endwhile; ?>
-		</main>
-		<?php get_sidebar(); ?>
+	<div class="row">
+		<div class="medium-8 columns">
+			<div class="main-grid">
+				<main class="main-content">
+					<?php while ( have_posts() ) : the_post(); ?>
+						<?php get_template_part( 'template-parts/content', '' ); ?>
+						<?php the_post_navigation(); ?>
+						<?php comments_template(); ?>
+					<?php endwhile; ?>
+				</main>
+				<?php get_sidebar(); ?>
+			</div>
+		</div>
 	</div>
 </div>
 <?php get_footer();
