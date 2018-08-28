b882eab7f3307d33d9bc0d1770a19a8c0a4df92e
Luego 08 - Rodrigo de Melo <rodrigodemelo@luego.com.br>
2018-08-28 12:07:51 -0300

    Working...

--- wp-content/themes/devinstall/header.php
+++ wp-content/themes/devinstall/header.php
@@ -25,8 +25,19 @@
           gtag('config', 'UA-124778545-1');
         </script>
 
+		<!-- Google Tag Manager -->
+		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+		'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+		})(window,document,'script','dataLayer','GTM-TXPRJ8F');</script>
+		<!-- End Google Tag Manager -->
 	</head>
 	<body <?php body_class(); ?>>
+		<!-- Google Tag Manager (noscript) -->
+		<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TXPRJ8F"
+		height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+		<!-- End Google Tag Manager (noscript) -->
 
 	<?php if ( get_theme_mod( 'wpt_mobile_menu_layout' ) === 'offcanvas' ) : ?>
 		<?php get_template_part( 'template-parts/mobile-off-canvas' ); ?>
