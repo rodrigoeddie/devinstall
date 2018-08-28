249dbc99059dbfc3cb335236f6d8f46939bd118d
Luego 08 - Rodrigo de Melo <rodrigodemelo@luego.com.br>
2018-08-28 11:52:37 -0300

    Working...

--- wp-content/themes/devinstall/header.php
+++ wp-content/themes/devinstall/header.php
@@ -15,6 +15,16 @@
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<?php wp_head(); ?>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-124778545-1"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-124778545-1');
+        </script>
+
 	</head>
 	<body <?php body_class(); ?>>
 
