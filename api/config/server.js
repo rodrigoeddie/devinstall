680869059e9518f47d5d6e25abba5f88780afe04
Rodrigo de Melo <rodrigoeddie@gmail.com>
2020-12-12 15:01:42 -0300

    Working...

--- api/config/server.js
+++ api/config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('APIPORT', 1337),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '25cbc5f984380e4e45ae5fd50e688fd6'),
